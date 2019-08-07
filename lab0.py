ed48b0604173aa49a0680c5660c378a940a4dbd2
1bm17cs074-ravi <53852194+1bm17cs074-ravi@users.noreply.github.com>
2019-08-07 12:31:33 +0530

    Update lab0.py

--- lab0.py
+++ lab0.py
@@ -1,18 +1,16 @@
+def maxi():
+    n = int(input())
+    a = []
+    for i in range(0,n):
+        temp = int(input())
+        a.append(temp)
+    for i in range(0,len(a)):
+        if i == 0:
+            maximum = a[i]
+        else:
+            if a[i]>maximum:
+                maximum = a[i]
+    print(str(maximum))
+        
 
-
-n = int(input("Enter the value of n:"))
-
-a = []
-print("Enter the numbers:")
-for i in range(0,n):
-    temp = int(input())
-    a.append(temp)
-
-for i in range(0,len(a)):
-    if i == 0:
-        max = a[i]
-    else:
-        if a[i]>max:
-            max = a[i]
-
-print("The maximum value:"+str(max)) 
+maxi()
