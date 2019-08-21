9da4bdb716d699ad192cbc87b961273f5c91e170
1bm17cs074-ravi <53852194+1bm17cs074-ravi@users.noreply.github.com>
2019-08-21 12:16:42 +0530

    Update kth_smallest_bub_sort.py

--- kth_smallest_bub_sort.py
+++ kth_smallest_bub_sort.py
@@ -1,16 +1,17 @@
 #bubble sort
 
-def bub_sort(a):
-    for i in range(0,len(a)-1):
+def bub_sort_kth_smallest(a,k):
+    for i in range(0,k):
         for j in range(0,len(a)-1-i):
             if a[j+1]<a[j]:
                 a[j],a[j+1] = a[j+1],a[j]
-    print(a)
 
 a = []
 print("Enter 5 elements into an array:")
 for i in range(0,5):
     t = int(input())
     a.append(t)
+    
+k = int(input("Enter the value of k:"))
 
-bub_sort(a)
+bub_sort_kth_smallest(a,k)
