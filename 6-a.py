9c7dc1896e4021718ed09cfa92b3799470f645a9
1bm17cs074-ravi <53852194+1bm17cs074-ravi@users.noreply.github.com>
2019-09-25 13:02:57 +0530

    Add files via upload

---
+++ 6-a.py
@@ -0,0 +1,31 @@
+from math import *
+import sys
+
+x = {}
+n = int(input("enter the value of n : "))
+
+def place(k, i):
+    if (i in x.values()):
+        return False
+    j = 1
+    while(j < k):
+        if abs(x[j]-i) == abs(j-k):
+            return False
+        j+=1
+    return True
+def clear_future_blocks(k):
+    for i in range(k,n+1):
+       x[i]=None
+def NQueens(k):
+    for i in range(1, n + 1):
+        clear_future_blocks(k)
+        if place(k, i):
+            x[k] = i
+            if (k==n):
+                print("The",n,"queens are placed in folloowing coordinates : ") 
+                for j in x:
+                    print((j,x[j]))
+                print('---------')
+            else:
+                NQueens(k+1)
+NQueens(1)
